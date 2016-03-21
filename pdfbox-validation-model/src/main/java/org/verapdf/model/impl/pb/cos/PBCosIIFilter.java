eb44c42e7d8e101e8220111ea86760380fd318f2
Timur Kamalov <timur.kamalov@duallab.com>
2016-03-21 17:19:08 +0300

    Fixed formatting.

--- pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/cos/PBCosIIFilter.java
+++ pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/cos/PBCosIIFilter.java
@@ -14,7 +14,8 @@ public class PBCosIIFilter extends PBCosName implements CosIIFilter {
 		super(filter, COS_II_FILTER_TYPE);
 	}
 
-	public PBCosIIFilter(final String filter) { this(COSName.getPDFName(filter));
+	public PBCosIIFilter(final String filter) {
+		this(COSName.getPDFName(filter));
 	}
 
 }
