8a458cdd589e6cc901c554e1477f4538fd4ba009
shem-sergey <sergey.shemyakov@duallab.com>
2016-06-30 18:18:04 +0300

    Fixed Perms bug.

--- pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/pd/PBoxPDDocument.java
+++ pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/pd/PBoxPDDocument.java
@@ -272,13 +272,6 @@ public class PBoxPDDocument extends PBoxPDObject implements PDDocument {
 			if (perms != null) {
 				List<PDPerms> list = new ArrayList<>(MAX_NUMBER_OF_ELEMENTS);
 				list.add(new PBoxPDPerms(perms));
-				/*for (COSBase perm : perms.getValues()) {
-					if (perm instanceof COSDictionary) {
-						list.add(new PBoxPDPerms((COSDictionary) perm));
-					} else if (perm instanceof COSObject && ((COSObject) perm).getObject() instanceof COSDictionary) {
-						list.add(new PBoxPDPerms((COSDictionary) ((COSObject) perm).getObject()));
-					}
-				}*/
 				return Collections.unmodifiableList(list);
 			}
 		}
