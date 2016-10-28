8c7307c4a4a46fb1e38c2fcfc06d350baeae13dc
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2016-10-28 15:00:49 +0300

    Added implementation for isAssociatedFile property of CosFileSpecification and fixes for Static Container

--- pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/external/PBoxEmbeddedFile.java
+++ pdfbox-validation-model/src/main/java/org/verapdf/model/impl/pb/external/PBoxEmbeddedFile.java
@@ -1,18 +1,21 @@
 package org.verapdf.model.impl.pb.external;
 
 import org.apache.log4j.Logger;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.cos.COSStream;
+import org.apache.pdfbox.cos.*;
 import org.verapdf.model.ModelParser;
 import org.verapdf.model.external.EmbeddedFile;
+import org.verapdf.model.impl.pb.containers.StaticContainers;
+import org.verapdf.model.impl.pb.pd.colors.PBoxPDSeparation;
+import org.verapdf.model.pdlayer.PDColorSpace;
 import org.verapdf.pdfa.PDFAValidator;
 import org.verapdf.pdfa.flavours.PDFAFlavour;
 import org.verapdf.pdfa.results.ValidationResult;
 import org.verapdf.pdfa.validators.Validators;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Embedded file representation implemented by Apache PDFBox
@@ -50,6 +53,7 @@ public class PBoxEmbeddedFile extends PBoxExternal implements EmbeddedFile {
 	public Boolean getisValidPDFA12() {
 		if (this.stream != null) {
 			try {
+				saveStaticContainersState();
 				InputStream unfilteredStream = stream.getUnfilteredStream();
 				unfilteredStream.mark(Integer.MAX_VALUE);
 				try (ModelParser parser1b = ModelParser.createModelWithFlavour(unfilteredStream,
@@ -57,6 +61,7 @@ public class PBoxEmbeddedFile extends PBoxExternal implements EmbeddedFile {
 					PDFAValidator validator1b = Validators.createValidator(PDFAFlavour.PDFA_1_B, false, 1);
 					ValidationResult result1b = validator1b.validate(parser1b);
 					if (result1b.isCompliant()) {
+						restoreSavedSCState();
 						return Boolean.TRUE;
 					}
 				}
@@ -65,13 +70,35 @@ public class PBoxEmbeddedFile extends PBoxExternal implements EmbeddedFile {
 						PDFAFlavour.PDFA_2_B)) {
 					PDFAValidator validator2b = Validators.createValidator(PDFAFlavour.PDFA_2_B, false, 1);
 					ValidationResult result2b = validator2b.validate(parser2b);
+					restoreSavedSCState();
 					return Boolean.valueOf(result2b.isCompliant());
 				}
 			} catch (Throwable e) {
 				LOGGER.debug("Exception during validation of embedded file", e);
+				restoreSavedSCState();
 				return Boolean.FALSE;
 			}
 		}
 		return Boolean.TRUE;
+	}
+
+	// We need to save data from StaticContainers before validating embedded documents
+	public Map<String, List<PBoxPDSeparation>> separations;
+	public List<String> inconsistentSeparations;
+	public Map<org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace, PDColorSpace> cachedColorSpaces;
+	public Set<COSObjectKey> fileSpecificationKeys;
+
+	private void saveStaticContainersState() {
+		this.separations = StaticContainers.separations;
+		this.inconsistentSeparations = StaticContainers.inconsistentSeparations;
+		this.cachedColorSpaces = StaticContainers.cachedColorSpaces;
+		this.fileSpecificationKeys = StaticContainers.fileSpecificationKeys;
+	}
+
+	private void restoreSavedSCState() {
+		StaticContainers.separations = this.separations;
+		StaticContainers.inconsistentSeparations = this.inconsistentSeparations;
+		StaticContainers.cachedColorSpaces = this.cachedColorSpaces;
+		StaticContainers.fileSpecificationKeys = this.fileSpecificationKeys;
 	}
 }
---
+++ pdfbox-validation-model/src/main/java/org/verapdf/model/tools/FileSpecificationKeysHelper.java
@@ -0,0 +1,244 @@
+package org.verapdf.model.tools;
+
+import org.apache.log4j.Logger;
+import org.apache.pdfbox.cos.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.common.PDStream;
+import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureElement;
+import org.apache.pdfbox.pdmodel.documentinterchange.logicalstructure.PDStructureNode;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType3Font;
+import org.apache.pdfbox.pdmodel.graphics.PDFontSetting;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObjectProxy;
+import org.apache.pdfbox.pdmodel.graphics.pattern.PDAbstractPattern;
+import org.apache.pdfbox.pdmodel.graphics.pattern.PDShadingPattern;
+import org.apache.pdfbox.pdmodel.graphics.pattern.PDTilingPattern;
+import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceDictionary;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceEntry;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAppearanceStream;
+import org.verapdf.model.impl.pb.containers.StaticContainers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Maksim Bezrukov
+ */
+public class FileSpecificationKeysHelper {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(FileSpecificationKeysHelper.class);
+
+	public static void registerFileSpecificationKeys(PDDocument document) {
+		PDDocumentCatalog catalog = document.getDocumentCatalog();
+		if (catalog != null) {
+			registerDictionaryAFKeys(catalog.getCOSObject());
+			processStructElements(catalog.getStructureTreeRoot());
+		}
+		PDPageTree pageTree = document.getPages();
+		if (pageTree != null) {
+			registerDictionaryAFKeys(pageTree.getCOSObject());
+			for (PDPage page : pageTree) {
+				processPage(page);
+			}
+		}
+	}
+
+	private static void processStructElements(PDStructureNode structureNode) {
+		if (structureNode != null) {
+			for (Object obj : structureNode.getKids()) {
+				if (obj instanceof PDStructureElement) {
+					PDStructureElement element = (PDStructureElement) obj;
+					registerDictionaryAFKeys(element.getCOSObject());
+					processStructElements(element);
+				}
+			}
+		}
+	}
+
+	private static void processPage(PDPage page) {
+		if (page != null) {
+			registerDictionaryAFKeys(page.getCOSObject());
+			try {
+				for (PDAnnotation annotation : page.getAnnotations()) {
+					if (annotation != null) {
+						registerDictionaryAFKeys(annotation.getCOSObject());
+						for (PDAppearanceStream stream : getAllAppearances(annotation)) {
+							processXObject(stream);
+						}
+					}
+				}
+			} catch (IOException e) {
+				LOGGER.error("Can not get page annotations", e);
+			}
+			parseResources(page.getResources());
+		}
+	}
+
+	private static void processXObject(PDXObject xObject) {
+		if (xObject != null) {
+			registerDictionaryAFKeys((COSDictionary) xObject.getCOSObject());
+			if (xObject instanceof PDFormXObject) {
+				parseResources(((PDFormXObject) xObject).getResources());
+			} else if (xObject instanceof PDImageXObjectProxy) {
+				try {
+					processXObject(((PDImageXObjectProxy) xObject).getMask());
+				} catch (IOException e) {
+					LOGGER.error("Can not obtain Image xobject Mask", e);
+				}
+				try {
+					processXObject(((PDImageXObjectProxy) xObject).getMask());
+				} catch (IOException e) {
+					LOGGER.error("Can not obtain Image xobject SMask", e);
+				}
+				processImageAlternates((PDImageXObjectProxy) xObject);
+			}
+		}
+	}
+
+	private static void processImageAlternates(PDImageXObjectProxy xObject) {
+		COSBase alternates = ((COSDictionary) xObject.getCOSObject()).getDictionaryObject(COSName.getPDFName("Alternates"));
+		if (alternates instanceof COSArray) {
+			for (COSBase obj : (COSArray) alternates) {
+				if (obj instanceof COSDictionary) {
+					COSBase image = ((COSDictionary) obj).getDictionaryObject(COSName.IMAGE);
+					if (image instanceof COSStream) {
+						processXObject(new PDImageXObjectProxy(new PDStream((COSStream) image), null));
+					}
+				}
+			}
+		}
+	}
+
+	private static List<PDAppearanceStream> getAllAppearances(PDAnnotation annotation) {
+		List<PDAppearanceStream> res = new ArrayList<>();
+		if (annotation != null) {
+			PDAppearanceDictionary appearance = annotation.getAppearance();
+			if (appearance != null) {
+				addAllAppearances(appearance.getNormalAppearance(), res);
+				addAllAppearances(appearance.getDownAppearance(), res);
+				addAllAppearances(appearance.getRolloverAppearance(), res);
+			}
+		}
+		return res;
+	}
+
+	private static void addAllAppearances(PDAppearanceEntry appearance, List<PDAppearanceStream> list) {
+		if (appearance.isStream()) {
+			PDAppearanceStream appearanceStream = appearance.getAppearanceStream();
+			if (appearanceStream != null) {
+				list.add(appearanceStream);
+			}
+		} else {
+			for (PDAppearanceStream appearanceStream : appearance.getSubDictionary().values()) {
+				if (appearanceStream != null) {
+					list.add(appearanceStream);
+				}
+			}
+		}
+	}
+
+	private static void registerDictionaryAFKeys(COSDictionary dictionary) {
+		if (dictionary == null) {
+			return;
+		}
+		COSBase af = dictionary.getDictionaryObject(COSName.getPDFName("AF"));
+		if (af instanceof COSArray) {
+			for (COSBase element : (COSArray) af) {
+				addElementKey(element);
+			}
+		}
+	}
+
+	private static void processExtGState(PDExtendedGraphicsState extGState) {
+		if (extGState == null) {
+			return;
+		}
+		PDFontSetting fontSetting = extGState.getFontSetting();
+		if (fontSetting != null) {
+			try {
+				processFont(fontSetting.getFont());
+			} catch (IOException e) {
+				LOGGER.error("Can not obtain font from extGState's font settings", e);
+			}
+		}
+	}
+
+	private static void processFont(PDFont font) {
+		if (font instanceof PDType3Font) {
+			parseResources(((PDType3Font) font).getResources());
+		}
+	}
+
+	private static void processPattern(PDAbstractPattern pattern) {
+		if (pattern instanceof PDTilingPattern) {
+			parseResources(((PDTilingPattern) pattern).getResources());
+		} else if (pattern instanceof PDShadingPattern) {
+			processExtGState(((PDShadingPattern) pattern).getExtendedGraphicsState());
+		}
+	}
+
+	private static void parseResources(PDResources resources) {
+		if (resources != null) {
+			parseResourcesXObjects(resources);
+			parseResourcesExtGState(resources);
+			parseResourcesPatterns(resources);
+			parseResourcesFonts(resources);
+		}
+	}
+
+	private static void parseResourcesPatterns(PDResources resources) {
+		for (COSName name : resources.getPatternNames()) {
+			try {
+				PDAbstractPattern pattern = resources.getPattern(name);
+				processPattern(pattern);
+			} catch (IOException e) {
+				LOGGER.error("Can not obtain pattern from resources", e);
+			}
+		}
+	}
+
+	private static void parseResourcesExtGState(PDResources resources) {
+		for (COSName name : resources.getExtGStateNames()) {
+			PDExtendedGraphicsState extGState = resources.getExtGState(name);
+			processExtGState(extGState);
+		}
+	}
+
+	private static void parseResourcesXObjects(PDResources resources) {
+		for (COSName name : resources.getXObjectNames()) {
+			try {
+				PDXObject xObject = resources.getXObject(name);
+				processXObject(xObject);
+			} catch (IOException e) {
+				LOGGER.error("Can not obtain xobject from resources", e);
+			}
+		}
+	}
+
+	private static void parseResourcesFonts(PDResources resources) {
+		for (COSName name : resources.getFontNames()) {
+			try {
+				PDFont font = resources.getFont(name);
+				processFont(font);
+			} catch (IOException e) {
+				LOGGER.error("Can not obtain font from resources", e);
+			}
+		}
+	}
+
+	private static void addElementKey(COSBase element) {
+		if (element != null) {
+			COSObjectKey key = element.getKey();
+			if (key != null) {
+				StaticContainers.fileSpecificationKeys.add(key);
+			}
+		}
+	}
+
+}
